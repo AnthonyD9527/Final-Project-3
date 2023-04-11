8e53ea2f043586e89c2522ad9979e85c5f0c6416
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-04-11 16:37:30 -0700

    installed nodemon package

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ const express = require("express")
 
 const app = express() 
 
-const port = 3005;
+app.get("/getConcerts", (req, res) => {
+    console.log("get all concerts")
+});
+console.log("test");
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log (`server is up and listening on port ${port}`)
 });
