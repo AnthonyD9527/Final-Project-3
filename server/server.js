a98a54efdf98d41389a8618664b2592e25d42604
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-04-11 18:11:54 -0700

    Added some middleware into code

--- server/server.js
+++ server/server.js
@@ -3,10 +3,32 @@ const express = require("express")
 
 const app = express() 
 
-app.get("/getConcerts", (req, res) => {
-    console.log("get all concerts")
+app.use((req, res, next) => {
+    console.log("yeah our middelware");
+    next();
+})
+
+app.get("/api/v1/concerts", (req, res) => {
+ res.status(404).json({
+    status: "sucess",
+    data: {
+        concert: ["Oldies", "Rap", "R&B"]
+
+    },
+ });
 });
-console.log("test");
+
+
+// Get a single Concert
+app.get("/api/v1/concerts/:concertid", (req, res) => {
+    console.log(req.params);
+} ) ;
+
+// How to create a single listing for Concert
+
+app.post("/api/v1/concerts", (req, res) => {
+    console.log(req);
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
