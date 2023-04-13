f94db26c3ecc23c2b564cdaebeb598ac34a3f9a3
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-04-12 17:20:34 -0700

    Added Delete, And Update Routes

--- server/server.js
+++ server/server.js
@@ -3,11 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
  
-
-app.use((req, res, next) => {
-    console.log("yeah our middelware");
-    next();
-});
+app.use(express.json())
 
 app.use((req, res, next) => {
     console.log("This is our second middelware");
@@ -16,7 +12,7 @@ app.use((req, res, next) => {
 
 app.get("/api/v1/concerts", (req, res) => {
  res.status(404).json({
-    status: "sucess",
+    status: "success",
     data: {
         concert: ["Oldies", "Rap", "R&B"]
 
@@ -26,15 +22,51 @@ app.get("/api/v1/concerts", (req, res) => {
 
 
 // Get a single Concert
-app.get("/api/v1/concerts/:concertid", (req, res) => {
+app.get("/api/v1/concerts/:id", (req, res) => {
     console.log(req.params);
+
+    res.status(200).json({
+        status: "success",
+        data: {
+            concert: "Oldies"
+        }
+    })
 } ) ;
 
 // How to create a single listing for Concert
 
 app.post("/api/v1/concerts", (req, res) => {
     console.log(req);
-});
+    res.status(201).json({
+        status: "success",
+        data: {
+            concert: "Oldies"
+        }
+    })
+} ) ;
+
+// Update 
+
+app.put("/api/v1/concerts/:id" , (req, res) => {
+console.log(req.params.id);
+console.log(req.body);
+res.status(200).json({
+    status: "success",
+    data: {
+        concert: "Oldies"
+    }
+})
+} ) ;
+
+// Delete
+
+app.delete("/api/v1/concerts/:id", (req, res) => {
+    res.status(204).json({
+        status: "success",
+        
+    })
+} ) ;
+
 
 
 const port = process.env.PORT || 3001;
