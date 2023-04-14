83bede681c6f927228d4b6e05584107abf533a22
AnthonyD9527 <106792017+AnthonyD9527@users.noreply.github.com>
2023-04-13 17:57:21 -0700

    Updated some of the Routes folders and added in a component folder with 3 new files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import ConcertDetail from "./routes/ConcertDetail";
 
 const App = () => {
-    return <div>app</div>;
-};
+    return (
+        
+          <div className="container">
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route
+                  exact
+                  path="/concerts/:id/update"
+                  component={UpdatePage}
+                />
+                <Route
+                  exact
+                  path="/concerts/:id"
+                  component={ConcertDetail}
+                />
+              </Switch>
+            </Router>
+          </div>
+        
+      );
+    };
 
-export default App;
+    export default App;
---
+++ client/src/routes/ConcertDetail.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ConcertDetail = () => {
+    return (
+        <div>
+            Detail Page
+
+        </div>
+    )
+}
+
+export default ConcertDetail;
